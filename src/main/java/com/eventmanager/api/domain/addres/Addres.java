f3e032b7947b87394198eaba60dcff1d4b336d19
otaviocostao <dev.otaviocostao@gmail.com>
2025-04-30 16:37:41 -0300

    feat: Entidades da api

---
+++ src/main/java/com/eventmanager/api/domain/addres/Addres.java
@@ -0,0 +1,31 @@
+package com.eventmanager.api.domain.addres;
+
+import com.eventmanager.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "addres")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Addres {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String cidade;
+
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "evento_id")
+    private Event event;
+}
