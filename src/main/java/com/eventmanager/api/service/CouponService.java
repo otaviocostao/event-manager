b5ebfd2d02a24114cdb3ae5291aa0a3837c7633f
otaviocostao <dev.otaviocostao@gmail.com>
2025-05-09 23:17:04 -0300

    feat: Coupon RequestDTO, Service, Controller e EndPoint para post

---
+++ src/main/java/com/eventmanager/api/service/CouponService.java
@@ -0,0 +1,34 @@
+package com.eventmanager.api.service;
+
+import com.eventmanager.api.domain.coupon.Coupon;
+import com.eventmanager.api.domain.coupon.CouponRequestDTO;
+import com.eventmanager.api.domain.event.Event;
+import com.eventmanager.api.repositories.CouponRepository;
+import com.eventmanager.api.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CouponService {
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+
+    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO data) {
+        Event couponEvent = eventRepository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("Event not found"));
+
+        Coupon newCoupon = new Coupon();
+        newCoupon.setCode(data.code());
+        newCoupon.setDiscount(data.discount());
+        newCoupon.setEvent(couponEvent);
+
+        return couponRepository.save(newCoupon);
+    }
+}
